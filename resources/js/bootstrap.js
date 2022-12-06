fc2a547f974adf1052b14bc08cdf98913c399bc1
baratahmed <baratahmed28@gmail.com>
2022-12-06 16:26:25 +0600

    First Commit

---
+++ resources/js/bootstrap.js
@@ -0,0 +1,76 @@
+window._ = require('lodash');
+
+/**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
+
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+const JwtToken = `Bearer ${localStorage.getItem('token')}`;
+window.axios.defaults.headers.common['Authorization'] = JwtToken;
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
+ */
+
+//For Pusher 
+import Echo from 'laravel-echo';
+
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: "75db49788874095b861f",
+    cluster: "ap2",
+    forceTLS: true,
+    encrypted: true,
+    auth:{
+        headers:{
+            Authorization: JwtToken
+        }
+    }
+});
+
+
+
+
+
+
+//For laravel web sockets 
+// import Echo from 'laravel-echo';
+
+// window.Pusher = require('pusher-js');
+
+// window.Echo = new Echo({
+//     authEndpoint : 'http://127.0.0.1:8000/broadcasting/auth',
+//     broadcaster: 'pusher',
+//     key: "myKey",
+//     wsHost: window.location.hostname,
+//     wsPort: 6001,
+//     forceTLS: false,
+//     disableStats: true,
+//     auth:{
+//         headers:{
+//             Authorization: JwtToken
+//         }
+//     }
+// });
